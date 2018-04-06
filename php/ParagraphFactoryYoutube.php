5a99c2d2378e7cfca00443ddcdd15d450f338282
patrickz98 <patrickz98@users.noreply.github.com>
2018-04-06 20:18:21 +0200

    Check in. 2018-04-06 20:18

---
+++ php/ParagraphFactoryYoutube.php
@@ -0,0 +1,51 @@
+<?php
+
+class ParagraphFactoryYoutube
+{
+    private static function createMedia($server, $auth, $url)
+    {
+        $url = "$server/entity/media?_format=json";
+
+        $media = [
+            "bundle" => [
+                [
+                    "target_id" => "video"
+                ]
+            ],
+            "field_media_video_embed_field" => [
+                [
+                    "value" => $url
+                ]
+            ]
+        ];
+
+        return Curl::post($url, $auth, $media);
+    }
+
+    public static function create($server, $auth, $tweet)
+    {
+        $media = self::createMedia($server, $auth, $tweet);
+        $targetId = $media[ "mid" ][ 0 ][ "value" ];
+
+        $data = [
+            "type" => [[
+                "target_id" => "video"
+            ]],
+            "field_media" => [
+                [
+                    "target_id" => $targetId
+                ]
+            ]
+        ];
+
+        $url = "$server/entity/paragraph?_format=json";
+        $paragraph = Curl::post($url, $auth, $data);
+
+        return [
+            "target_id" => $paragraph[ "id" ][ 0 ][ "value" ],
+            "target_revision_id" => $paragraph[ "revision_id" ][ 0 ][ "value" ]
+        ];
+    }
+}
+
+?>
