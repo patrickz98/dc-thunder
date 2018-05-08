4e073c85fd490d0413129a28fbc50dba42181251
patrickz98 <patrickz98@users.noreply.github.com>
2018-05-08 11:12:37 +0200

    Check in. 2018-05-08 11:12

--- Templates-DCX/post.sh
+++ Templates-DCX/post.sh
@@ -1,19 +1,11 @@
 #!/bin/sh
 
-# curl \
-#     -v \
-#     --url "http://192.168.18.131/dcx/atom/documents" \
-#     --user "testuser:dc" \
-#     --header "Content-Type: application/atom+xml;type=entry" \
-#     --data-binary @post.xml > zzz.txt
-
 curl \
     -v \
     --url "http://192.168.18.131/dcx/api/document" \
     --user "testuser:dc" \
     --header "Content-Type: application/json" \
     --data-binary @story-post.json > zzz.txt
-    # --data-binary @post.xml > zzz.txt
 
 cat zzz.txt
 
